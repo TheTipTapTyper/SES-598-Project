0d6fd1af618b3477da8af8936f509856a07f6acc
Josh Martin <jmmartin397@protonmail.com>
2022-04-19 15:31:11 -0700

    update

--- ros_drone_controller.py
+++ ros_drone_controller.py
@@ -5,6 +5,7 @@ from geometry_msgs.msg import PoseStamped, Point, Quaternion, Twist
 from sensor_msgs.msg import Image
 import math
 import numpy as np
+from numpy.random import default_rng
 
 from mavros_msgs.srv import CommandBool, SetMode
 from std_msgs.msg import String
@@ -29,10 +30,15 @@ COUNTER_TURN = 'CounterTurn'    # Counter turn for a random duration
 SEEK_CAR = 'SeekCar'            # Move towards car until in center of view
 FINISHED = 'Finished'           # hover indefintely
 
+TARGET_ALTITUDE = 15 # meters
 TURNS_PER_DIRECTION = 5
+MAX_VELOCITY = 1 # m/s
 MAX_DELTA_THETA = .15 # rad/s
 MAX_COUNTER_TURN_DURATION = 5 # sec
+MIN_COUNTER_TURN_DURATION = 2 # sec
 NUM_PRIOR_PREDICTIONS = 3
+DISTANCE_THRESHOLD = 1 # meters
+DELTA_THETA_DECAY_RATE = 0.01 # % per step
 
 class DroneController:
     def __init__(self):
@@ -40,32 +46,37 @@ class DroneController:
         rospy.Subscriber('/mavros/local_position/pose', PoseStamped, 
             callback=self._pose_callback)
         rospy.Subscriber('/mavros/state', State, callback=self._state_callback)
-        #rospy.Subscriber('/mavros/local_', State, callback=self._state_callback)
         rospy.Subscriber('/uav_camera_down/image_raw', numpy_msg(Image), callback=self._image_callback)
+        #rospy.Subscriber('/mavros/local_', State, callback=self._state_callback)
         #self.pose_pub = rospy.Publisher('/mavros/setpoint_position/local', PoseStamped, queue_size=10)
         self.vel_pub = rospy.Publisher('/mavros/setpoint_velocity/cmd_vel_unstamped', Twist, queue_size=10)
         rospy.wait_for_service(SET_MODE_SRV)
         rospy.wait_for_service(CMD_ARMING_SRV)
         self.set_mode_service = rospy.ServiceProxy(SET_MODE_SRV, SetMode)
         self.cmd_arming_service = rospy.ServiceProxy(CMD_ARMING_SRV, CommandBool)
-        # self.des_pose = PoseStamped()
-        # self.position = (0, 0, 0)
-        # self.velocity = (0, 0, 0)
-        # self.angular_velocity = (0, 0, 0)
-        # self.angles = (0, 0, 0)
+        self.x_pos = self.y_pos = self.z_pos = 0 # meters
+        self.x_vel = self.y_vel = self.z_vel = 0 # m/s
+        self.delta_theta = 0 # rad/s
         self.is_ready = False
         self.rate = rospy.Rate(RATE)
         self.mode = None
         self.is_armed = False
+        self.rng = default_rng()
+
+    def ensure_correct_mode(self):
+        if self.mode != CUSTOM_MODE:
+            try:
+                self.set_mode_service(1, CUSTOM_MODE)
+            except rospy.ServiceException as e:
+                print('/mavros/set_mode service call failed: ', e)
+        if not self.is_armed:
+            try:
+                self.cmd_arming_service(True)
+            except rospy.ServiceException as e:
+                print('/mavros/cmd/arming service call failed: ', e)
 
     def _image_callback(self, msg):
-        self.image = np.frombuffer(msg.data, dtype=np.uint8).reshape(msg.height, msg.width, -1)
-
-    @property
-    def camera_view(self):
-        """ current view of the downward facing drone camera.
-        """
-        pass
+        self.camera_view = np.frombuffer(msg.data, dtype=np.uint8).reshape(msg.height, msg.width, -1)
 
     @property
     def is_over_lot(self):
@@ -84,10 +95,11 @@ class DroneController:
         return (sum(self.prior_predictions) / NUM_PRIOR_PREDICTIONS) >= .5
 
     def _pose_callback(self, pose):
-        pass
-        # pos = pose.pose.position
+        pos = pose.pose.position
+        self.x_pos = pos.x
+        self.y_pos = pos.y
+        self.z_pos = pos.z
         # quat = pose.pose.orientation
-        # self.position = (pos.x, pos.y, pos.z)
         # angles = (euler_from_quaternion([quat.x, quat.y, quat.z, quat.w]))
         # self.angles = tuple(rad2deg(angle) for angle in angles)
 
@@ -96,35 +108,37 @@ class DroneController:
         self.is_armed = msg.armed
         #print('mode: {} armed: {}'.format(msg.mode, msg.armed))
 
-    # def _velocity_callback(self, msg):
-    #     print(msg)
-
     def update_state(self):
-        pass
-        # if self.state == GO_STRAIGHT:
-        #     if not self.is_over_lot:
-        #         if self.turns_since_dir_change > TURNS_PER_DIRECTION:
-        #             self.turn_direction *= -1
-        #             self.turns_since_dir_change = 0
-        #         self.turns_since_dir_change += 1
-        #         self.state = FIND_LOT
-        #         self.delta_theta = MAX_DELTA_THETA
-        # elif self.state == FIND_LOT:
-        #     if self.is_over_lot:
-        #         self.state = COUNTER_TURN
-        #         self.counter_steps_to_go = self.rng.integers(low=MIN_COUNTER_TURN_STEPS, 
-        #             high=MAX_COUNTER_TURN_STEPS, endpoint=True)
-        #         self.delta_theta = -MAX_COUNTER_TURN_DELTA_THETA
-        #     else: # gradually widen the spiral
-        #         self.delta_theta *= (1 - DELTA_THETA_DECAY_RATE)
-        # elif self.state == COUNTER_TURN:
-        #     if self.counter_steps_to_go > 0:
-        #         self.counter_steps_to_go -= 1
-        #     else:
-        #         self.state = GO_STRAIGHT
-        #         self.delta_theta = 0
-        # else:
-        #     raise ValueError('Invalid state: {}'.format(self.state))
+        if self.state == INIT:
+            if TARGET_ALTITUDE - self.y_pos < DISTANCE_THRESHOLD:
+                self.state = GO_STRAIGHT
+                self.y_vel = 0
+                self.x_vel = MAX_VELOCITY
+        elif self.state == GO_STRAIGHT:
+            if not self.is_over_lot:
+                if self.turns_since_dir_change > TURNS_PER_DIRECTION:
+                    self.turn_direction *= -1
+                    self.turns_since_dir_change = 0
+                self.turns_since_dir_change += 1
+                self.state = FIND_LOT
+                self.delta_theta = MAX_DELTA_THETA
+        elif self.state == FIND_LOT:
+            if self.is_over_lot:
+                self.state = COUNTER_TURN
+                self.counter_turn_duration = self.rng.uniform(
+                    MIN_COUNTER_TURN_DURATION, MAX_COUNTER_TURN_DURATION
+                )
+                self.delta_theta = -MAX_DELTA_THETA
+            else: # gradually widen the spiral
+                self.delta_theta *= (1 - DELTA_THETA_DECAY_RATE)
+        elif self.state == COUNTER_TURN:
+            if self.counter_steps_to_go > 0:
+                self.counter_steps_to_go -= 1
+            else:
+                self.state = GO_STRAIGHT
+                self.delta_theta = 0
+        else:
+            raise ValueError('Invalid state: {}'.format(self.state))
 
     def step(self):
         """ Move forward one time step. Update state based on segmenter output
@@ -132,10 +146,16 @@ class DroneController:
         """
         self.update_state()
         self.move()
-        self.path.append((self.x, self.y, self.theta))
+        #self.path.append((self.x, self.y, self.theta))
 
     def run(self):
-        pass
+        self.ensure_correct_mode()
+        self.MODE = INIT
+        while(1):
+            self.step()
+            self.rate.sleep()
+
+
 
 def distance(pos1, pos2):
     total = 0
